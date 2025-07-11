eb1962bf2e39757d8c19fbaa884097f1c9175935
Georg Kaltenbrunner <kaltenbrunnergeorg@googlemail.com>
2025-07-11 11:19:56 +0200

    data faker final

--- src/fake_data_generation/GetFakeData.py
+++ src/fake_data_generation/GetFakeData.py
@@ -1,11 +1,11 @@
 import fake_data_utils;
 
-# Generierung
-customers_df = fake_data_utils.generate_customers(1000)
+# Generierung von 50.000 Fake Kund:innen und 500.000 Fake Transkationen
+customers_df = fake_data_utils.generate_customers(50_000)
 products_df = fake_data_utils.generate_products()
-transactions_df = fake_data_utils.generate_transactions(customers_df, products_df, 1000)
+transactions_df = fake_data_utils.generate_transactions(customers_df, products_df, 500_000)
 
 
-# customers_df.to_csv("data/raw/customers.csv", index=False)
-# products_df.to_csv("data/raw/products.csv", index=False)
+customers_df.to_csv("data/raw/customers.csv", index=False)
+products_df.to_csv("data/raw/products.csv", index=False)
 transactions_df.to_csv("data/raw/transactions.csv", index=False)
